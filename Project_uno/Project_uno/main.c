b509dc57a6b43c513aea6c7389ddff6843a5f89e
nnelimarkka <nikkeneli@gmail.com>
2022-05-04 18:47:55 +0300

    add passive buzzer

--- Project_uno/Project_uno/main.c
+++ Project_uno/Project_uno/main.c
@@ -12,6 +12,9 @@
 #define BAUD 9600
 #define MYUBRR (FOSC/16/BAUD-1)
 
+#define TRUE 1
+#define FALSE 0
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include <util/setbaud.h>
@@ -20,16 +23,21 @@
 
 volatile uint8_t spi_recv;
 
+volatile uint8_t buzzerEnable = FALSE;
+
+void PWM();
+
 //LED on pin7 for now
 void completeAction(uint8_t command)
 {
 	switch (command)
 	{
 		case 20:
-			PORTD |= (1 << PD7);
+			buzzerEnable = TRUE;
+			PWM();
 			break;
 		case 19:
-			PORTD &= ~(1 << PD7);
+			buzzerEnable = FALSE;
 			break;
 		default:
 			break;
@@ -40,6 +48,46 @@ ISR (SPI_STC_vect)
 {
 	spi_recv = SPDR;
 	completeAction(spi_recv);
+}
+
+ISR (TIMER1_COMPA_vect)
+{
+	TCNT1 = 0; // reset timer counter
+}
+
+void PWM()
+{
+	
+	DDRB |= (1 << PB1); // OC1A is located in digital pin 9
+	
+	// Enable interrupts
+	sei();
+	
+	
+	TCCR1B  = 0; 
+	TCCR1A  = 0;
+	TCNT1   = 0;
+	
+	TCCR1A |= (1 << 6); 
+	
+	
+	TCCR1A |= (1 << 0); 
+	TCCR1B |= (1 << 4); 
+	
+	TIMSK1 |= (1 << 1); // enable compare match A interrupt
+	
+	OCR1A = 2440;  
+	OCR1B = 500;
+	
+	while(buzzerEnable == TRUE)
+	{
+		/* enable timer/counter1 */
+		TCCR1B |= (1 << 0); // set prescaling to 1 (no prescaling)
+		
+	}
+	TIMSK1 &= ~(1 << 1); // disable compare match A interrupt
+	
+	return;
 }
 
 int
